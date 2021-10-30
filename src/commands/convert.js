500e0be8edeedd724cfd4e0dc9577c199d75f8d9
Akal-Ustat Singh <akalustat.singh@gmail.com>
2021-10-30 14:25:19 -0700

    Test fluent ffmpeg conversions

--- src/commands/convert.js
+++ src/commands/convert.js
@@ -6,26 +6,26 @@ import { getFileName, validatePath } from '../utils/files.js'
 import { argError, endWithError } from '../utils/error.js'
 
 const program = new Command()
-
 program
-  .argument('<path>', 'path to file')
-  .argument('<output>', 'output location, otherwise it will use the current directory', process.cwd)
-  .argument('[name]', 'optionally change the name')
-  .option('-t, --type <filetype>'  , 'what type of file is this? img or audio', 'audio' )
-  .option('-f, --format <output-format>', 'override the default conversion format (m4a for audio and jpg for images)')
-  .action((path, outputPath, newName, options) => {
+  .argument('<filepath>', 'path to file')
+  .option('-t, --type <filetype>', 'img or audio', 'audio')
+  .option('-f, --format <output-format>', 'override the default conversion format (m4a for audio and jpg for images')
+  .option('-p, --output-path', 'path to write file to', process.cwd)
+  .option('-n, --name', 'new name')
+  .action((path, options) => {
     validatePath(path)
-    const {type: fileType} = options
-    
-    switch(fileType) {
+    const {type: fileType, name: newName, outputPath } = options
+
+    switch(filetype) {
       case 'audio': {
         const {format: outputFormat = 'm4a'} = options
         const fileName = newName || getFileName(path)
 
-        validatePath(`${outputPath}`)
-        ffmpeg()
-          .input(path)
-          .on('error', (err) => {
+
+        validatePath(outputPath)
+
+        ffmpeg(path) 
+        .on('error', (err) => {
             endWithError(`Something went wrong with ffmpeg\n${err}`)
           })
           .on('progress', (data) => {
@@ -39,17 +39,59 @@ program
         break
       }
       case 'img': {
-        const {format: outputFormat = 'jpg'} = options
-        const fileName = newName || getFileName(path)
-
-        sharp(path)
-          .toFile(`${outputPath}/${fileName}.${outputFormat}`)
         break
       }
       default: {
-        argError('File type provided for conversion was invalid')
         break
       }
     }
   })
+
+
 program.parse(process.argv)
+// program
+//   .argument('<path>', 'path to file')
+//   .argument('<output>', 'output location, otherwise it will use the current directory', process.cwd)
+//   .argument('[name]', 'optionally change the name')
+//   .option('-t, --type <filetype>'  , 'what type of file is this? img or audio', 'audio' )
+//   .option('-f, --format <output-format>', 'override the default conversion format (m4a for audio and jpg for images)')
+//   .action((path, outputPath, newName, options) => {
+//     validatePath(path)
+//     const {type: fileType} = options
+    
+//     switch(fileType) {
+//       case 'audio': {
+//         const {format: outputFormat = 'm4a'} = options
+//         const fileName = newName || getFileName(path)
+
+//         validatePath(`${outputPath}`)
+//         ffmpeg()
+//           .input(path)
+//           .on('error', (err) => {
+//             endWithError(`Something went wrong with ffmpeg\n${err}`)
+//           })
+//           .on('progress', (data) => {
+//               const { currentFps,  currentKbps, targetSize, timemark, percent} = data
+//               console.log(`${percent}% done. Currently at ${timemark}, working at ${currentKbps} on the way to ${targetSize} `)
+//           })
+//           .on('end', () => {
+//             console.log('finished conversion')
+//           })
+//           .saveToFile(`${outputPath}/${fileName}.${outputFormat}`)
+//         break
+//       }
+//       case 'img': {
+//         const {format: outputFormat = 'jpg'} = options
+//         const fileName = newName || getFileName(path)
+
+//         sharp(path)
+//           .toFile(`${outputPath}/${fileName}.${outputFormat}`)
+//         break
+//       }
+//       default: {
+//         argError('File type provided for conversion was invalid')
+//         break
+//       }
+//     }
+//   })
+// program.parse(process.argv)
